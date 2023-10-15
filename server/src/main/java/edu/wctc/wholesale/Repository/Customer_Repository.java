4cff3038aef4f19fd640ba908baad3996af685c0
Mike-Karis <77794142+Mike-Karis@users.noreply.github.com>
2023-10-14 22:32:12 -0500

    Delete server directory

--- server/src/main/java/edu/wctc/wholesale/Repository/Customer_Repository.java
+++
@@ -1,8 +0,0 @@
-package edu.wctc.wholesale.Repository;
-
-import edu.wctc.wholesale.Entity.Wholesale_order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface Customer_Repository extends CrudRepository<Wholesale_order, Integer> {
-
-}
